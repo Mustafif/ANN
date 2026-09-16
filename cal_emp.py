c5beed15c9e4cbc888e066f3abab5498bcc1f259
Mustafif <mustafif0929@gmail.com>
2026-09-15 21:14:05 -0400

    update aic/bic

--- cal_emp.py
+++ cal_emp.py
@@ -184,8 +184,8 @@ def initial_guess(log_returns, r):
     print(f"Loss: {loss}")
     k = len(params)
     n = len(log_returns)
-    aic = 2 * k - 2 * loss
-    bic = k * np.log(n) - 2 * loss
+    aic = 2 * k + 2 * loss
+    bic = k * np.log(n) + 2 * loss
 
     print(f"AIC: {aic}")
     print(f"BIC: {bic}")
